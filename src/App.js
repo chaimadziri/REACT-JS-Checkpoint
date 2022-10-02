f195dad4862d882600e4a21a4b8945956c9ed896
chaimadziri <dziri.chaima1@gmail.com>
2022-10-02 12:30:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (  
+    <div className="App">
+    <form>
+    <div class="row">
+  <div class="col">
+  <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
+  </div>
+  <div class="col">
+  <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"/>
+  </div>
+  </div>
+          <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            address
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
